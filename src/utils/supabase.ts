1cc3395c6ccc52d3b8305a2bc2cffcec7a5b15bd
VINAY KUMAR P <vinaykumar.p1995v@gmail.com>
2025-07-06 18:55:38 +0530

    fix(console): removing console.log

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -4,7 +4,5 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || '';
 const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY || '';
 
- ('supabaseUrl', supabaseUrl, supabaseKey)
-
 export const supabase = createClient( supabaseUrl, supabaseKey);
         
